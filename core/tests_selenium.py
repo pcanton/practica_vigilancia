f9a36baf1721c57b98a8b00565a5503f67799534
pcanton <pcanton1123@gmail.com>
2026-06-03 15:30:11 +0200

    Fix: Afegit requirements i corregit test per a Fase GREEN

--- core/tests_selenium.py
+++ core/tests_selenium.py
@@ -3,18 +3,16 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class SecurityRegressionTests(TestCase):
-    fixtures = ['testdb.json']  # Càrrega de dades (Punt 2.2.2 del PDF)
+    fixtures = ['testdb.json']  # Càrrega de dades obligatòria
 
     def test_role_restriction(self):
         """AUDITORIA: L'analista no ha d'entrar a /admin/"""
-        # 1. Realitzem el Login simulat utilitzant l'usuari de les nostres dades del JSON
-        # Passem l'usuari 'analista1' i la seva credencial
+        # 1. Realitzem el Login simulat de l'usuari analista
         self.client.login(username='analista1', password='PasswordSegur123!')
         
         # 2. Intentem forçar l'accés a la URL d'administració protegida
         response = self.client.get('/admin/', follow=True)
         
-        # 3. ASSERT DE SEGURETAT (Fase RED - Forcem el FAIL per a l'Evidència 2)
-        # Comprovem si ha pogut entrar responent amb un codi d'èxit 200 de l'admin
-        # Això fallarà (FAIL) perquè actualment l'analista té els permisos trets i el sistema el redirigirà
-        self.assertEqual(response.status_code, 200)
+        # 3. ASSERT DE SEGURETAT (Fase GREEN - El test ha de passar)
+        # Verifiquem que l'analista NO rep un codi de visualització exitós (200) de l'admin
+        self.assertNotEqual(response.status_code, 200)
